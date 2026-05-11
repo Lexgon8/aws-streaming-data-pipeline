ea98a252458b9b13a8989797bf6b8d182e3f3fbb
Lexgon8 <alexgon8@gmail.com>
2026-05-11 07:53:50 -0300

    Update lambda_function.py

--- lambda/consumer/lambda_function.py
+++ lambda/consumer/lambda_function.py
@@ -1,35 +1,32 @@
-import json
-import boto3
-import base64
-from datetime import datetime
-
-s3_client = boto3.client('s3')
-
-BUCKET_NAME = 'globalsupplydata'
-
 def lambda_handler(event, context):
 
     for record in event['Records']:
 
         kinesis_data = record['kinesis']['data']
         decoded_data = base64.b64decode(kinesis_data).decode('utf-8')
-        
 
         try:
             payload = json.loads(decoded_data)
+
         except json.JSONDecodeError as e:
-            print(f"Erro ao decodificar o JSON: {e}")
+            print(f"Erro ao decodificar JSON: {e}")
             continue
-        
 
-        current_date = datetime.utcnow().strftime('%Y/%m/%d')
-        
+        now = datetime.utcnow()
 
-        file_name = f"iot_data/{current_date}/sensor_data_{datetime.utcnow().strftime('%H%M%S')}.json"
-        
+        year = now.strftime("%Y")
+        month = now.strftime("%m")
+        day = now.strftime("%d")
+
+        file_name = (
+            f"raw/iot/"
+            f"year={year}/"
+            f"month={month}/"
+            f"day={day}/"
+            f"evento.json"
+        )
 
         file_content = json.dumps(payload)
-        
 
         s3_client.put_object(
             Bucket=BUCKET_NAME,
@@ -37,9 +34,7 @@ def lambda_handler(event, context):
             Body=file_content
         )
 
-
     return {
         'statusCode': 200,
         'body': json.dumps('Data processed and saved to S3')
     }
-
